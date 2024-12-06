85a5c107bb81d8110068bb6f0cfe3303d723cb6c
Arthur K <arthurk031@outlook.com>
2024-12-05 21:19:04 -0300

    Início com primeira avaliação

---
+++ Fatoriais.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TUAvaliacao
+{
+    public class Fatoriais
+    {
+        public int CalcularFatorial(int numero)
+        {
+            int resultado = 1;
+            if (numero < 0)
+            {
+                throw new ArgumentException("O número fatorial não pode ser negativo.");
+            }
+            else if (numero == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    resultado *= i;
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ TUAvaliacao.Tests/Fatoriais.Test.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+
+namespace TUAvaliacao.Tests
+{
+    public class CalculoFatoriaisTest
+    {
+        private readonly Fatoriais _sut;
+
+        public CalculoFatoriaisTest()
+        {
+            _sut = new Fatoriais();
+        }
+
+        [Theory]
+        [InlineData(5, 120)]
+        [InlineData(3, 6)]
+        [InlineData(1, 1)]
+        public void CalcularFatorial_DeveRetornarResultado_QuandoNumeroValido(int numero,
+                                                                              int expectedResult)
+        {
+            // Act
+            int result = _sut.CalcularFatorial(numero);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void CalcularFatorial_DeveRetornarUm_QuandoNumeroEhZero()
+        {
+            // Act
+            int result = _sut.CalcularFatorial(0);
+            int expectedResult = 1;
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void CalcularFatorial_DeveEmitirException_QuandoNumeroNegativo()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _sut.CalcularFatorial(-1));
+        }
+    }
+}
